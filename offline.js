8472eda75da22f14e1e0b68be72b72b0677b577d
Pakhum <pakhum949@gmail.com>
2018-09-14 23:04:23 +0700

    sts.26708-5

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1536939305,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/body-sheet0.png",
+		"images/sprite18-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/particles.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/johnj_wafare_gun_pistol_silenced_walther_pp7.m4a",
+		"media/johnj_wafare_gun_pistol_silenced_walther_pp7.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
